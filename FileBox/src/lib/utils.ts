38c6eb8121ff2ca6b54a59abd3c556b228688811
Mehrshad <code.rezaei@gmail.com>
2024-10-05 05:15:50 +0330

    refactor(utils): path handling & bytesToSize
    - update bytesToSize function to return '0B' instead of '0 Byte'
    - enhance joinPaths to handle both forward and backslashes consistently
    - fix getParentDirectory logic for root paths on windows

--- FileBox/src/lib/utils.ts
+++ FileBox/src/lib/utils.ts
@@ -1,6 +1,6 @@
 function bytesToSize(bytes?: number) {
     if (bytes === 0 || bytes === undefined) {
-        return "0 Byte";
+        return "0B";
     }
     const sizes = ["B", "KB", "MB", "GB", "TB"];
     const i = Math.floor(Math.log(bytes) / Math.log(1024));
@@ -8,16 +8,18 @@ function bytesToSize(bytes?: number) {
 }
 
 const joinPaths = (...paths: string[]): string => {
+    const separator = paths.some(path => path.includes("\\")) ? "\\" : "/"
     return paths
-        .join(paths.includes("\\") ? "\\" : "/")
-        .replace(/\\/g, "/")
-        .replace(/\/+/g, "/")
-        .replace(/\/$/, "");
-};
+        .join(separator)
+        // This normalizes the path by removing redundant separators.
+        .replace(/[\\/]+/g, separator)
+        // This ensures the path doesn't end with a separator.
+        .replace(/[\\/]$/, "")
+}
 
 function getParentDirectory(path: string): string {
     const isRoot = path === "/" || /^[a-zA-Z]:[\\]?$/.test(path);
-    if (isRoot) return path.includes("\\") ? path : path + "\\";
+    if (isRoot) return path;
 
     const parts = path.split(/[/\\]/);
     parts.pop();
